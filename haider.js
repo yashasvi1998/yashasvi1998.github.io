04505934ab4ac4b17ba2754433978058f038c975
Yashasvi Shukla <yashasvishukla72@gmail.com>
2018-06-18 14:52:04 +0530

    upload

---
+++ haider.js
@@ -0,0 +1,14 @@
+var button = document.querySelectorAll("button")[1];
+var btn = document.querySelector(".navbar-brand");
+button .addEventListener("click",function(){
+		button.style.cursor="pointer";
+		alert("Happy Birthday Haider");
+		
+		
+});
+
+
+btn.addEventListener("click",function(){
+		btn.style.cursor="pointer";
+		alert("Arey Ha Bhai Happy Birthday Haider!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+});
